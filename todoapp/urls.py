ab0d23b925d666446f34eb53a8867f4a564b43ff
Sabera <saberaakterpapri08@gmail.com>
2021-11-11 20:14:41 +0600

    Item List

---
+++ todoapp/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+from main import views
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('add_todo/', views.add_todo),
+    path('delete_todo/<int:todo_id/>', views.delete_todo),
+]
---
+++ todoapp/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from main.models import Todo
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+
+
+def home(request):
+    todo_items = Todo.objects.all().order_by("-added_date")
+    return render(request, "main/index.html", {"todo_items": todo_items})
+
+
+@csrf_exempt
+def add_todo(request):
+    print(resquest.POST)
+    current_date = timezone.now()
+    content = request.POST("content")
+    created_obj = Todo.objects.create(added_date=current_date, text=content)
+    print(added_date)
+    print(content)
+    print(created_obj)
+    print(created_obj.id)
+    lenght_of_todos = Todo.objects.all().count()
+    print(lenght_of_todos)
+
+    return HttpResponseRedirect("/")
+
+
+@csrf_exempt
+def delete_todo(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    print(todo_id)
+    return HttpResponseRedirect("/")
